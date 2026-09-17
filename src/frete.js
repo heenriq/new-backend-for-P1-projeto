a6bdc03904c7d5493cecac8e5c905ff7154f0495
Pedro H. Santos <pedrohsantosfx@gmail.com>
2026-09-16 21:24:55 -0300

    adicionado cors e novos valores ao frete.

--- src/frete.js
+++ src/frete.js
@@ -1,5 +1,8 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 const frete = 5;
@@ -7,12 +10,12 @@ const frete = 5;
 export function calcularFrete(valorPedido) {
   if (isNaN(valorPedido) || valorPedido < 0) {
     return { tipo: "inválido", valorFinal: "Valor do pedido inválido." };
-  } else if (valorPedido <= 5) {
+  } else if (valorPedido <= 7) {
     return {
       tipo: "frete",
       valorFinal: valorPedido + frete,
     };
-  } else if (valorPedido > 5 && valorPedido <= 9) {
+  } else if (valorPedido > 7 && valorPedido <= 10) {
     return {
       tipo: "descontado",
       valorFinal: valorPedido + frete / 2,
